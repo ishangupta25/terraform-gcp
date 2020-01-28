ba306b89d8ebc44c7174d309d3525e4b9221782b
ishangupta25 <ishangupta25@gmail.com>
2020-01-28 23:02:35 +0530

    Update main.tf

--- modules/private_instance/main.tf
+++ modules/private_instance/main.tf
@@ -32,7 +32,7 @@ resource "google_compute_instance" "hosts" {
   metadata_startup_script = "${data.template_file.start-up.rendered}"
   
   metadata {
-    ssh-keys               = "ishan:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuSSnfSjgxJXjZ8AZu/9me96ZG2rYcpJ3sPACi8ra0BZjWUfXAmAXdzIcYbu54UqoxiesP1sObng2w75zOJXfEKViQ6iqT2zLkOo5TMN9X4+j1wwKjX0fRC4ewnRTHCHsUvrtXhYBl5UGpA72EbUxq0K6NPVNIBBkDaYrwaAXgAKvLyMEK2i0cbxbKAybbEI1haWzUFv913+f+XGxaKdxyGJagrRhJNUdhULfpJTitmhCJco6zU3vRv4UisPURK6DNk5jbeDREGO9GkpvcOEqib1OwQ1vneNKaSMf3c/xs3j8fYKp7AQBEKH77VuFAvcqQzBvIq9y2klkhFCH5XhAH root@232eb2c1ec17"
+    ssh-keys               = "ishan:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCuSSnfSjgxJXjZ8AZu/9me96ZG2rYcpJ3sPACi8ra0BZjWUfXAmAXdzIcYbu54UqoxiesP1sObng2w75zOJXfEKViQ6iqT2zLkOo5TMN9X4+j1wwKjX0fRC4ewnRTHCHsUvrtXhYBl5UGpA72EbUxq0K6NPVNIBBkDaYrwaAXgAKvLyMEK2i0cbxbKAybbEI1haWzUFv913+f+XGxaKdxyGJagrRhJNUdhULfpJTitmhCJco6zU3vRv4UisPURK6DNk5jbeDREGO9GkpvcOEqib1OwQ1vneNKaSMf3c/xs3j8fYKp7AQBEKH77VuFAvcqQzBvIq9y2klkhFCH5 root@232eb2c1ec17"
   }
 
   network_interface {
